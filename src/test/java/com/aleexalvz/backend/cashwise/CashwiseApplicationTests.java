03ec41bd729ce54d88a7d61edf4a37a743c813a2
Alex Alves <alex.alves_br@hotmail.com>
2023-05-15 20:45:33 -0300

    init project

---
+++ src/test/java/com/aleexalvz/backend/cashwise/CashwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleexalvz.backend.cashwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
